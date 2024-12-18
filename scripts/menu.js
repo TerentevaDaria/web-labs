3916f5bdaf283955f32af6d9fa2fe22c859a8929
Daria <you@example.com>
2024-12-18 03:37:05 +0300

    lab4 fix

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.menu__item').forEach(link => {
-        link.href === document.location.href
+        (link.href === document.location.href || link.href.endsWith("index.html") && document.location.href.endsWith("/"))
             ? link.classList.add('menu__item_active')
             : link.classList.remove('menu__item_active');
     });
